379a5515e775e02cb0d0e746d311d50fa0ab9606
Tarek Chebbi <tarek.philipp.chebbi@gmail.com>
2018-02-19 22:42:02 +0100

    Removed output from git checker

--- scripts/git-check-status.sh
+++ scripts/git-check-status.sh
@@ -2,10 +2,10 @@
 
 git remote update > /dev/null
 
-UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
+UPSTREAM=${1:-'@{u}'} > /dev/null
+LOCAL=$(git rev-parse @) > /dev/null
+REMOTE=$(git rev-parse "$UPSTREAM") > /dev/null
+BASE=$(git merge-base @ "$UPSTREAM") > /dev/null
 
 if [ $LOCAL = $REMOTE ]; then
 	exit 0 #Up-to-date
