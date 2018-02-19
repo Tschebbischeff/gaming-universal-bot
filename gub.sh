77f390d45bf44d86a50412b36fffd74f41eafbbc
Tarek Chebbi <tarek.philipp.chebbi@gmail.com>
2018-02-19 20:40:52 +0100

    debug

--- gub.sh
+++ gub.sh
@@ -9,15 +9,16 @@ pIdNode=$(jobs -p | sed '1!d')
 pIdLiveShell=$(jobs -p | sed '2!d')
 kill -STOP $pIdLiveShell
 sh ./scripts/git-update-checker.sh $pIdNode $pIdLiveShell &
+pIdUpdateChecker=$(jobs -p | sed '3!d')
 sleep 10
 echo '=== Live shell ==='
 kill -CONT $pIdLiveShell
-%2
+fg '%2'
 echo 'Live Shell terminated, checking for update-script'
 if [ ! -f /saved/git-perform-update.sh ]; then
     echo 'Live Shell was terminated by user... Terminating'
-    kill -TERM %1
-    kill -TERM %3
+    kill -TERM $pIdNode
+    kill -TERM $pIdUpdateChecker
 else
     exec ./saved/git-perform-update.sh
 fi
